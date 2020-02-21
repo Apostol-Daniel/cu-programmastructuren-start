a1c46e16b1d3a13734a8ba571136bc7d92e7ade3
Bart Soete <bart.soete@howest.be>
2020-02-21 13:45:22 +0100

    Add Layout and Vars

--- js/script.js
+++ js/script.js
@@ -3,16 +3,76 @@
 window.addEventListener('load', Initieer);
 
 // Globale Variabelen
+var divToekenningResult;
+var divVergelijkResult;
+var divBoolResult;
+var divIfResult;
+var divSwitchResult;
+var divWhileResult;
+var divDoWhileResult;
+var divForResult;
+var divBreakResult;
+var divContinueResult;
+
+var txtSwitchInput;
+var txtWhileInput;
+var txtDoWhileInput;
+var txtForInput;
 
 
 
-// Functies
+
+
+
 function Initieer(){
   // Lokale variabelen
+  let btnToekenning;
+  let btnVergelijk;
+  let btnBool;
+  let btnIf;
+  let btnWhile;
+  let btnDoWhile;
+  let btnFor;
+  let btnForIn;
+  let btnBreak;
+  let btnContinue;
+
 
   // DOM objecten ophalen
+  divToekenningResult = document.querySelector('#divToekenningResult');
+  divVergelijkResult = document.querySelector('#divVergelijkResult')
+  divWiskundeResult = document.querySelector('#divWiskundeResult');
+  divBoolResult = document.querySelector('#divBoolResult');
+  divIfResult = document.querySelector('#divIfResult');
+  divSwitchResult = document.querySelector('#divSwitchResult');
+  divWhileResult = document.querySelector('#divWhileResult');
+  divDoWhileResult = document.querySelector('#divDoWhileResult');
+  divForResult = document.querySelector('#divForResult');
+  divForInResult = document.querySelector('#divForInResult');
+  divBreakResult = document.querySelector('#divBreakResult');
+  divContinueResult = document.querySelector('#divContinueResult');
 
+  btnToekenning = document.querySelector('#btnToekenning');
+  btnVergelijk = document.querySelector('#btnVergelijk');
+  btnWiskunde = document.querySelector('#btnWiskunde');
+  btnBool = document.querySelector('#btnBool');
+  btnIf = document.querySelector('#btnIf');
+  btnSwitch = document.querySelector('#btnSwitch');
+  btnWhile = document.querySelector('#btnWhile');
+  btnDoWhile = document.querySelector('#btnDoWhile');
+  btnFor = document.querySelector('#btnFor');
+  btnForIn = document.querySelector('#btnForIn');
+  btnBreak = document.querySelector('#btnBreak');
+  btnContinue = document.querySelector('#btnContinue');
+
+  txtSwitchInput = document.querySelector('#txtSwitchInput');
+  txtWhileInput = document.querySelector('#txtWhileInput');
+  txtDoWhileInput = document.querySelector('#txtDoWhileInput');
+  txtForInput = document.querySelector('#txtForInput');
   // Eventlisteners voorzien
 
 
 }
+
+
+// Functies
